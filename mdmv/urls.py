9ae7b1d3ee29f828f93495f2dfd3e5d04a32bb97
donggrant <gdd7jh@virginia.edu>
2020-09-17 10:13:15 -0400

    Created empty app

--- mdmv/urls.py
+++ mdmv/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('donations/', include('donations.urls')),
+    path('', include('donations.urls')),
     path('admin/', admin.site.urls),
 ]
