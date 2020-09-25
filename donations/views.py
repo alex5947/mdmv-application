16266cd58e583c803578a37d576fe8e32a4d4ec7
Alex Chan <alexc5947@gmail.com>
2020-09-25 00:54:58 -0400

    added a navbar

--- donations/views.py
+++ donations/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 
+from django.views import generic
+
+from .models import Donation
+
 
 def index(request):
     return HttpResponse("Welcome to the Donations Page!")
+
+class DonationView(generic.ListView):
+    template_name = 'donations/navbar.html'
+
+    def get_queryset(self):
+        return Donation.objects.all()
