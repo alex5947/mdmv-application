9306b55900c7f40711b0d0fecfcf51cebd603d8a
Cam D <cjd6ff@virginia.edu>
2020-10-23 09:56:01 -0400

    Updated heroku import

--- mdmv/settings.py
+++ mdmv/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -149,4 +148,9 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
